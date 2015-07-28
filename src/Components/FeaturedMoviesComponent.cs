2c252e95228ca7b03f6370c2bf07b17dccf7e5c3
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-28 22:36:44 +0200

    Add FeaturedMovies custom component

---
+++ src/Components/FeaturedMoviesComponent.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Caching;
+using Microsoft.Framework.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomTagHelper.Services;
+using CustomTagHelper.Models;
+
+namespace CustomTagHelper.Components
+{
+    [ViewComponent(Name = "FeaturedMovie")]
+    public class FeaturedMoviesComponent : ViewComponent
+    {
+        private readonly IMemoryCache _cache;
+        private readonly MovieService _movieService;
+
+        public FeaturedMoviesComponent(MovieService movieService, IMemoryCache cache)
+        {
+            _movieService = movieService;
+            _cache = cache;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            // Since this component is invoked from within a CacheTagHelper,
+            // cache the movie list and provide an expiration trigger, which when triggered causes the
+            // CacheTagHelper's cached data to be invalidated.
+            var cacheKey = "featured_movies";
+            IEnumerable<FeaturedMovie> movies;
+            if (!_cache.TryGetValue(cacheKey, out movies))
+            {
+                IExpirationTrigger trigger;
+                movies = _movieService.GetFeaturedMovie(out trigger);
+                _cache.Set(cacheKey, movies, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            }
+            return View(movies);
+        }
+
+        public IViewComponentResult Invoke(string movieName)
+        {
+            string quote;
+            if (!_cache.TryGetValue(movieName, out quote))
+            {
+                IExpirationTrigger trigger;
+                quote = _movieService.GetCriticsQuote(out trigger);
+                _cache.Set(movieName, quote, new MemoryCacheEntryOptions().AddExpirationTrigger(trigger));
+            }
+            return Content(quote);
+        }
+    }
+}
